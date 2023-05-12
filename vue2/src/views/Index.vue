19557dae2be6008a887ad1169495e682a2d69264
_Wh0ami <newEmail>
2023-05-12 16:09:45 +0800

    2023.5.12.16:09

---
+++ vue2/src/views/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="index_container">
+      <div id="map_wrap">
+          <map-container></map-container>
+      </div>
+  </div>
+</template>
+
+<script>
+import MapContainer from '@/components/MapContainer.vue'
+export default {
+  components: { MapContainer },
+
+}
+</script>
+
+<style lang="less" scoped>
+#index_container{
+    height: 100%;
+    width: 100%;
+    position: relative;
+}
+#map_wrap{
+    height: 60%;
+    width: 60%;
+}
+</style>

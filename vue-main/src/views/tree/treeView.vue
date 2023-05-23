580cf693c18fed8a137f3bb3e987f35aa0b8058e
_Wh0ami <newEmail>
2023-05-23 10:27:26 +0800

    2023.5.23

--- vue-main/src/views/tree/treeView.vue
+++ vue-main/src/views/tree/treeView.vue
@@ -5,84 +5,69 @@
 </template>
 
 <script>
+import { createTree } from "@/api/node.js";
+
 export default {
+  data() {
+    return {
+      treeData: null,
+    }
+  },
+
+  methods: {
+    loadData() {
+      createTree().then((response) => {
+        this.treeData = response.data.data;
+        const tree = this.treeData;
+
+        const canvas = this.$refs.canvas;
+        const ctx = canvas.getContext("2d");
+
+        const nodeDistanceX = 100;
+        const nodeDistanceY = 80;
+
+        const nodeColor = "red";
+        const pathColor = "red";
+
+        const drawNode = (node, x, y, level) => {
+          ctx.fillStyle = nodeColor;
+          ctx.beginPath();
+          ctx.arc(x, y, 20, 0, 2 * Math.PI);
+          ctx.stroke();
+          ctx.fill();
+          ctx.fillStyle = "white";
+          ctx.fillText(node.value, x - 5, y + 5);
+
+          if (node.left) {
+            const x1 = x - nodeDistanceX / Math.pow(2, level + 1);
+            const y1 = y + nodeDistanceY;
+            ctx.strokeStyle = pathColor;
+            ctx.beginPath();
+            ctx.moveTo(x, y + 20);
+            ctx.lineTo(x1, y1 - 20);
+            ctx.stroke();
+            drawNode(node.left, x1, y1, level + 1);
+          }
+
+          if (node.right) {
+            const x2 = x + nodeDistanceX / Math.pow(2, level + 1);
+            const y2 = y + nodeDistanceY;
+            ctx.strokeStyle = pathColor;
+            ctx.beginPath();
+            ctx.moveTo(x, y + 20);
+            ctx.lineTo(x2, y2 - 20);
+            ctx.stroke();
+            drawNode(node.right, x2, y2, level + 1);
+          }
+        };
+
+        drawNode(tree, 400, 50, 0);
+      })
+    },
+  },
+
   mounted() {
-    const canvas = this.$refs.canvas;
-    const ctx = canvas.getContext("2d");
-
-    // 定义二叉树的数据结构
-    const tree = {
-      value: 10,
-      left: {
-        value: 5,
-        left: {
-          value: 2,
-          left: null,
-          right: null
-        },
-        right: {
-          value: 7,
-          left: null,
-          right: null
-        }
-      },
-      right: {
-        value: 15,
-        left: {
-          value: 12,
-          left: null,
-          right: null
-        },
-        right: {
-          value: 20,
-          left: null,
-          right: null
-        }
-      }
-    };
-
-    // 定义节点之间的水平距离和垂直距离
-    const nodeDistanceX = 100;
-    const nodeDistanceY =80;
-
-    // 定义节点和路径的颜色
-    const nodeColor = "red";
-    const pathColor = "red";
-
-    // 绘制二叉树
-    const drawNode = (node, x, y, level) => {
-      ctx.fillStyle = nodeColor;
-      ctx.beginPath();
-      ctx.arc(x, y, 20, 0, 2 * Math.PI);
-      ctx.stroke();
-      ctx.fill();
-      ctx.fillStyle = "white";
-      ctx.fillText(node.value, x - 5, y + 5);
-
-      if (node.left) {
-        const x1 = x - nodeDistanceX / Math.pow(2, level + 1);
-        const y1 = y + nodeDistanceY;
-        ctx.strokeStyle = pathColor;
-        ctx.beginPath();
-        ctx.moveTo(x, y + 20);
-        ctx.lineTo(x1, y1 - 20);
-        ctx.stroke();
-        drawNode(node.left, x1, y1, level + 1);
-      }
-
-      if (node.right) {
-        const x2 = x + nodeDistanceX / Math.pow(2, level + 1);
-        const y2 = y + nodeDistanceY;
-        ctx.strokeStyle = pathColor;
-        ctx.beginPath();
-        ctx.moveTo(x, y + 20);
-        ctx.lineTo(x2, y2 - 20);
-        ctx.stroke();
-        drawNode(node.right, x2, y2, level + 1);
-      }
-    };
-
-    drawNode(tree, 400, 50, 0);
+    this.loadData();
   }
 };
 </script>

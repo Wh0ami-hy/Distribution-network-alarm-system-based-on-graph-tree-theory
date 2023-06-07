b8716eb168da565551cd7a5a04464509feb730c4
_Wh0ami <newEmail>
2023-06-07 16:49:01 +0800

    2023.6.7

--- vue-main/src/views/dashboard/index.vue
+++ vue-main/src/views/dashboard/index.vue
@@ -22,7 +22,7 @@ export default {
   &-container {
     height: 93vh;
     
-    background-image: url('https://th.bing.com/th/id/R.355a077ecfeda012588d1307ac9a7800?rik=W3KqcYV2K6Wsrg&riu=http%3a%2f%2fup.iosdesk.com%2fpic_source%2f35%2f5a%2f07%2f355a077ecfeda012588d1307ac9a7800.jpg&ehk=3ZU%2b%2bAFCoH7iSioblUWR4l0xC6998LvC0so4L%2b6cPLY%3d&risl=&pid=ImgRaw&r=0');
+    background-image: url('../../assets/404_images/background.jpg');
     background-size: cover;
     background-position: center;
   }
